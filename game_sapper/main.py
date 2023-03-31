6e410bbcfb3736c17f835e3a634668e239bb63dc
Aleksey <vivera83@yandex.ru>
2023-03-31 16:52:06 +0300

    Добавлено сообщение на случай если в переменных окружение незаписаны вашные переменные

--- game_sapper/main.py
+++ game_sapper/main.py
@@ -3,4 +3,16 @@ from core.app import make_app
 
 if __name__ == "__main__":
     app = make_app()
-    run_app(app, host=app.settings.host, port=app.settings.port)
+    if all([app.settings.postgres.db,
+            app.settings.postgres.user,
+            app.settings.postgres.password,
+            app.settings.postgres.db_schema]):
+        run_app(app, host=app.settings.host, port=app.settings.port)
+    else:
+        app.logger.error(f"""
+                One of the required parameters is not specified in the environment variables:
+                POSTGRES__DB
+                POSTGRES__USER
+                POSTGRES__PASSWORD
+                POSTGRES__DB_SCHEMA
+        """)
